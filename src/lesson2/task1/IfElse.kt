435dd315dd6bead88cfdd7785b90f88271fc636b
loleckek228 <george-valiev@yandex.ru>
2017-10-06 22:43:09 +0300

    Я сделяль

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -124,15 +124,12 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
 fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
     if (a <= c && b <= d && b >= d)
         return b - c
+    else if (a <= c && b >= d)
+         return d - c
+    else if (a >= c && b >= d && a <= d)
+         return d - a
+    else if (a >= c && b <= d)
+         return b - a
     else
-        if (a <= c && b >= d)
-            return d - c
-        else
-            if (a >= c && b >= d && a <= d)
-                return d - a
-            else
-                if (a >= c && b <= d)
-                    return b - a
-                else
-                    return -1
+         return -1
 }
